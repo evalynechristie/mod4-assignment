48b6da31933efdf56bf0dde9e32e2afc32f0b1ed
evalynechristie <85860685+evalynechristie@users.noreply.github.com>
2021-07-08 12:01:23 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+
+yaakovGreeter.sayHello();
+johnGreeter.sayHi();
+
+// Immediately Invoked Function Expression
+// IIFE
+(function (name) {
+  console.log("Hello " + name);
+})("Coursera!");
+
+
+(function (name) {
+  console.log("Hello " + name);
+})("Paul");
+
+
+(function (name) {
+  console.log("Good bye " + name);
+})("Joe");
+
+
+(function (name) {
+  console.log("Good bye " + name);
+})("Janet");
+
+
+(function (name) {
+  console.log("Good bye " + name);
+})("Julie");
+ 
+
+(function (name) {
+  console.log("Hello " + name);
+})("Amos");
+
+
+(function (name) {
+  console.log("Hello " + name);
+})("Raul");
+
+
+(function (name) {
+  console.log("Hello " + name);
+})("Daniell");
